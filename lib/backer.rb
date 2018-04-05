f7aba5aecdf49dd4c05b2813ea1db2bdfb6c40dd
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-05 15:40:35 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,8 +6,11 @@ class Backer
   def initialize(name)
     @name = name
     @backed_projects = []
-
   end
 
+  def back_project(project)
+    project = Project.new(project)
+    self.backed_projects << project
+  end
 
 end
